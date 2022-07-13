1b1971a14036fb94d257bed1f00c5f5bb3aa8099
Anderson Santiago <andersonsantiago749@gmail.com>
2022-07-13 11:23:15 -0300

    feat: add implementation find all Users

--- src/main/java/com/example/sistemaJava/controller/UsersController.java
+++ src/main/java/com/example/sistemaJava/controller/UsersController.java
@@ -3,16 +3,25 @@ package com.example.sistemaJava.controller;
 import com.example.sistemaJava.dto.User;
 import com.example.sistemaJava.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
 public class UsersController {
     @Autowired
     private UserService service;
+
+
+    @GetMapping
+    public List<User> findAll() {
+        return service.findUsers();
+    }
 
     @PostMapping(path = "/create")
     public User sendCreateUser(@RequestBody User user) {
--- src/main/java/com/example/sistemaJava/service/UserService.java
+++ src/main/java/com/example/sistemaJava/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.sistemaJava.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -12,5 +14,9 @@ public class UserService {
 
     public User saveProduct(User user) {
         return repository.save(user);
+    }
+
+    public List<User> findUsers(){
+        return repository.findAll();
     }
 }
